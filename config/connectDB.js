b80eae800de8ae34cbe9bd2faadff2cccff84568
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-02-21 10:39:44 -0600

    Fix config

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Connect to the database
 function connectDB() { 
     mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
         useNewUrlParser: true,
